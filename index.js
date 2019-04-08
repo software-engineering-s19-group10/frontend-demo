4f293f048c26c70778a9f45515bd493c2a306cd3
mok3112 <mok3112@gmail.com>
2019-04-08 18:19:04 -0400

    Added redirection to the main page after successful login, and redirected to login page if user has not logged in yet

--- index.js
+++ index.js
@@ -2,15 +2,34 @@ import HomeView from './modules/home.js'
 import VisitorView from './modules/visitor.js'
 import StatisticsView from './modules/statistics.js'
 
-const modules = {
-  'home': new HomeView(),
-  'visitor': new VisitorView(),
-  'statistics': new StatisticsView(),
-};
+if (sessionStorage.getItem("token") == null) {
+  // Workaround for redirection so we can test without deploying. 
+  // Uses the fact that window.location.hostname is "" for local files.
+  if (window.location.hostname == "") {
+    // Running the front end locally, so have to change the absolute path.
+    console.log("running locally");
+    const location = window.location.pathname;
+    const directory = location.substring(0, location.lastIndexOf('/'));
+    const newPath = directory + "/login.html";
+    window.location.href = newPath;
+  } else {
+    // Front end is hosted somewhere, so use a relative path.
+    console.log("not running locally");
+    window.location.href = "login.html";
+  }
+} else {
+  const modules = {
+    'home': new HomeView(),
+    'visitor': new VisitorView(),
+    'statistics': new StatisticsView(),
+  };
 
-const view_items = document.getElementById('view-items');
+  const view_items = document.getElementById('view-items');
 
-for (let module in modules)
-  view_items.appendChild(modules[module].menuElement);
+  for (let module in modules)
+    view_items.appendChild(modules[module].menuElement);
 
-document.body.appendChild(modules['home'].mainElement);
+  document.body.appendChild(modules['home'].mainElement);
+
+}
+
--- login/login.js
+++ login/login.js
@@ -45,8 +45,34 @@ class LoginView {
         headers: {
           'Content-Type': 'application/json'
         }
-      }).then(response => response.json()).then(responseJson =>
-        console.log(JSON.stringify(responseJson)));
+      })
+        .then(response => response.json())
+        .then(responseJson => {
+          console.log(responseJson);
+          if (responseJson.hasOwnProperty("token")) {
+            // Successful login.
+            sessionStorage.setItem("token", responseJson.token);
+
+            // Workaround for redirection so we can test without deploying. 
+            // Uses the fact that window.location.hostname is "" for local files.
+            if (window.location.hostname == "") {
+              // Running the front end locally, so have to change the absolute path.
+              console.log("running locally");
+              const location = window.location.pathname;
+              const directory = location.substring(0, location.lastIndexOf('/'));
+              const newPath = directory + "/index.html";
+              window.location.href = newPath;
+            } else {
+              // Front end is hosted somewhere, so use a relative path.
+              console.log("not running locally");
+              window.location.href = "index.html";
+            }
+
+          } else {
+            // Unsuccessful login. Display error message.
+
+          }
+        })
     });
     main.appendChild(loginButton);
   }
