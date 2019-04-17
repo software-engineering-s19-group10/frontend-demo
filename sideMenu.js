cfffc0556c8f0c90a9b9b692c6c7b49e37a70a1e
DanNguyen-CE <43036823+DanNguyen-CE@users.noreply.github.com>
2019-04-17 18:00:56 -0400

    Mobile styling & improved sidebar functionality

--- sideMenu.js
+++ sideMenu.js
@@ -8,46 +8,46 @@ window.onload = function() {
   }; console.log("Mobile:", isMobileDevice()); 
 
   if (isMobileDevice()){ // Mobile only!
-
-    var main = document.getElementById('main-view');
     var sideMenu = document.getElementById('side-menu');
-    var current, isAnimating;
-
-    // Main view default position when loaded in mobile view (NEED FIX : ONLY CHANGES 1 OF THE MODULES)
-    main.style.left = '-180px';
-    main.style.position = 'relative';
-    main.style.zIndex = '-1'; // Behind the side menu
-    main.style.width = '300px'; // Set width. (NEED FIX: SHOULD FILL ENTIRE WIDTH)
+    var items = document.getElementById('view-items');
+    var current, cancelAnimation;
 
     // Side menu default position when loaded in mobile view
-    sideMenu.style.left = '-180px'; 
+    sideMenu.style.left = '-180px';
 
     sideMenu.addEventListener('touchend', function(ev) { // End touch event listener
-      if (parseInt(sideMenu.style.left) > -90) { // Opens menu when moved to over 50%
-        window.requestAnimationFrame(animateOpen);
-      }
+      cancelAnimation = false;
 
-      if (parseInt(sideMenu.style.left) <= -90) { // Closes menu when moved to under 50%
-        window.requestAnimationFrame(animateClose);
-      }
+      if (parseInt(sideMenu.style.left) > -90) // Opens menu when moved to over 50%
+        animateOpen();
+
+      if (parseInt(sideMenu.style.left) <= -90) // Closes menu when moved to under 50%
+        animateClose();
     })
 
     sideMenu.addEventListener('touchmove', function(ev) { // Active touch event listener
       current = ev.targetTouches[0]; // Updates current touch position
+      
       window.requestAnimationFrame(moveWithFinger);
+      cancelAnimation = true;
+    })
+
+    items.addEventListener('click', function() {
+      animateClose();
     })
   }
 
   function animateOpen() { // Menu opening animation
-    isAnimating = true;
     let pos = parseInt(sideMenu.style.left);
     let id = setInterval(frame, 1);
 
     function frame() { // Animate
       if (pos == 0) {
         clearInterval(id);
-        isAnimating = false;
       } else {
+        if (cancelAnimation) {
+          clearInterval(id);
+        }
         pos++;
         sideMenu.style.left = pos + "px";
       }
@@ -55,15 +55,16 @@ window.onload = function() {
   }
 
   function animateClose() { // Menu closing animation
-    isAnimating = true;
     let pos = parseInt(sideMenu.style.left);
     let id = setInterval(frame, 1);
   
     function frame() { // Animate
       if (pos == -180) {
         clearInterval(id);
-        isAnimating = false;
       } else {
+        if (cancelAnimation) {
+          clearInterval(id);
+        }
         pos--; 
         sideMenu.style.left = pos + "px";
       }
@@ -71,7 +72,7 @@ window.onload = function() {
   }
 
   function moveWithFinger() { // Moves the side menu with the finger.
-    if (parseInt(sideMenu.style.left) >= -180 && parseInt(sideMenu.style.left) <= 0 && !isAnimating)
+    if (parseInt(sideMenu.style.left) >= -180 && parseInt(sideMenu.style.left) <= 0)
       sideMenu.style.left = (current.clientX - sideMenu.offsetWidth) + 'px';
 
     if (parseInt(sideMenu.style.left) < -180) // Prevents over shoot
