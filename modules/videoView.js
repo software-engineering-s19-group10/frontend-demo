134fce1ac97c7317c202726869157fcd3601eee1
mnadev <mohammednadeem902@gmail.com>
2019-04-21 21:08:40 -0400

    refactored code to implement new methods update and clear

--- modules/videoView.js
+++ modules/videoView.js
@@ -27,7 +27,6 @@ export default class videoView extends BaseView {
     const playButton = document.createElement("button");
     playButton.type = "button";
     playButton.id = "play-btn";
-    playButton.value = "Pause"
     playButton.textContent = "Pause";
 
     playButton.style.width = '50em'; // setting the width 
@@ -36,10 +35,133 @@ export default class videoView extends BaseView {
    
     wait("#play-btn");
 
-    const streaming_script = document.createElement("script");
-    streaming_script.type = "text/javascript";
-    streaming_script.src = "helper_scripts/client_streaming.js";
+    const rewind = document.createElement("button");
+    rewind.type = "button";
+    rewind.id = "rewind";
+    rewind.textContent = "Rewind 10 sec.";
 
-    this.mainElement.appendChild(streaming_script);
+    rewind.style.width = '50em'; // setting the width 
+    rewind.style.height = '5em'; // setting the height 
+    this.mainElement.appendChild(rewind);
+   
+    wait("#rewind");
+
   }
+
+  /* Fill out view with fresh information */
+  update() { 
+
+    function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    // Set port and URL
+    let PORT = 8080;
+
+    var frames = [];
+
+    let authToken = sessionStorage.getItem("token");
+
+    fetch('https://boiling-reef-89836.herokuapp.com/lock_owners/api/locks/?Authorization=' + authToken, {
+        method: 'GET',
+        params: JSON.stringify({"Authorization": authToken}),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(responseJson => {
+        console.log(responseJson);
+        var URL = responseJson[0]["ip_address"];
+        console.log(URL);
+        // testing purposes
+        // URL = "127.0.0.1:8080";
+        
+        // Create the socket
+        console.log("Creating Socket.");
+        this.socket = new WebSocket('ws://' + URL + ":" + PORT);
+        console.log("Socket Created.");
+
+
+        // Event Handler for opening the connection
+        this.socket.onopen = function(event) {
+            console.log("Connected.");
+        }
+
+        
+
+        // Boolean to check if we should render the image or not. I.E. is it paused?
+        var isPlaying = true;
+
+        // Get play button from DOM.
+        let play_btn = document.getElementById("play-btn");
+
+        // Toggle play on click of button
+        play_btn.addEventListener("click", function() {
+            console.log("Changing from isPlaying:" + isPlaying + "to " + !isPlaying + ".");
+            // Change the icon
+            if(isPlaying) {
+                play_btn.textContent = "Play";
+            } else {
+                play_btn.textContent = "Pause";
+            }
+
+            // Toggle between play and pause
+            isPlaying = !isPlaying;
+        });
+
+
+        // Get play button from DOM.
+        let rewind = document.getElementById("rewind");
+
+        // Toggle play on click of button
+        rewind.addEventListener("click", function() {
+           
+           
+        });
+
+        // Get the image element from the DOM
+        let imgelement = document.getElementById("latestImage");
+        //console.log(imgelement);
+
+        // Event handler for receiving a message.
+        this.socket.onmessage = function(event) {
+
+            console.log("Frame received.");
+
+            // Get the frame which is an encoded JPEG image.
+            let frameSTR = event.data;        
+
+            // Add headers for the JPEG
+            console.log("Adding JPEG headers");
+            var datajpg = "data:image/jpg;base64," + frameSTR;
+            //console.log(datajpg);
+            console.log("JPEG Headers Added.");
+            
+            // append the frame to the array 
+            frames.push(datajpg);
+
+
+            if(frames.length > 10) {
+              frames.shift();
+            }
+
+            if(isPlaying) {
+                console.log("Rendering Image.");
+                // Display on screen.
+                imgelement.src = datajpg;
+            }
+        }
+        
+    });
+  }
+
+  /* Return main to it's starting state */
+  clear() { 
+     console.log("Closing connection.");
+     this.socket.close();
+  }
+
+
+
 }
